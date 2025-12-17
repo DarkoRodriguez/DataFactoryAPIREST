5a2a67a9b8ed02d29c4eca7bdd706bdcbed5ed55
DarkoRodriguez <dark.rodriguez@duocuc.cl>
2025-12-17 00:47:08 -0300

    se

--- src/main/java/com/example/miapp/repository/OrdenRepository.java
+++ src/main/java/com/example/miapp/repository/OrdenRepository.java
@@ -1,11 +1,10 @@
 package com.example.miapp.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.miapp.model.Orden;
-
-
-import java.util.Optional;
 
 public interface OrdenRepository extends JpaRepository<Orden, Long> {
 	Optional<Orden> findByNumeroOrden(String numeroOrden);
